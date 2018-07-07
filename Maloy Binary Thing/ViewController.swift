791484606e22edb3a38089a3b6c23249b9bccc1c
Anthony Li <li.anthony917@gmail.com>
2018-07-07 17:56:16 -0400

    Initial Commit

---
+++ Maloy Binary Thing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maloy Binary Thing
+//
+//  Created by Anthony Li on 7/7/18.
+//  Copyright © 2018 Anthony Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Maloy Binary ThingTests/Maloy_Binary_ThingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Maloy_Binary_ThingTests.swift
+//  Maloy Binary ThingTests
+//
+//  Created by Anthony Li on 7/7/18.
+//  Copyright © 2018 Anthony Li. All rights reserved.
+//
+
+import XCTest
+@testable import Maloy_Binary_Thing
+
+class Maloy_Binary_ThingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
